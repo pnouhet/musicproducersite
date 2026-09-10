bcc0167fc37eaf6f7287b9dbb504a4fb361b1771
Pierre <nouhet.pierre@gmail.com>
2026-09-10 16:11:25 +0200

    fix declarations globales

--- globals.d.ts
+++ globals.d.ts
@@ -1,7 +1,5 @@
 // Déclarations de modules pour les imports de fichiers de style à effet de bord
 declare module "*.css";
-declare module "shader-art";
-declare module "@shader-art/plugin-uniform";
 
 // Librairies dont les types ne sont pas résolus via leur champ "exports"
 declare module "shader-art";
